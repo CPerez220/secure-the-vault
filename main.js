e2cfc8088e1b954e64f1924a2be797a386ad3d36
Carlos Perez <cmperez0730@gmail.com>
2024-03-04 13:04:14 -0800

    Finish the project

---
+++ main.js
@@ -0,0 +1,14 @@
+//First, I put a string in a variable.
+const string = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
+//My first variable use a add ecuation to get 10.
+const variable1 = 5 + 5;
+//My second variable use a substraction to get 20.
+const variable2 = 60 - 20;
+//My third variable use a very easy multiply ecuation to get 39.
+const variable3 = 39 * 1;
+//All the results of this variable from the code to open the vault.
+
+//Next, I use "alert" to show the message in form of a notification to the user.
+//I put the message in () and put the variables in ${}.
+//alert(`${string}${variable1} ${variable2} ${variable3}`)
+//I put this in the index.html page so when you click the buttom you will recieve the message.
